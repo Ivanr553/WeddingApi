3a29219a9759de2374e7cd52731401ba5c3b37c8
Ivan <ivanr553@gmail.com>
2025-03-20 13:22:26 -0700

    udpating to ust jsonbin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import weddingList from './wedding_list.json'
+export interface Env {
+	API_HOST: string;
+	ApiKey: string;
+	BinId: string
+}
 
+interface IBin {
+	RSVP: Guest[];
+}
 
 interface Guest {
 	name: string;
@@ -20,9 +27,26 @@ export default {
 			children: 0
 		};
 
-		(weddingList.list as Guest[]).push(testData);
+		const BIN_ID = env.BinId;
+		const API_KEY = env.ApiKey;
+		const URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
+		const getBinRequest = await fetch(URL);
+		const bin = await getBinRequest.json() as IBin;
 
-		return new Response();
+		bin.RSVP.push(testData)
+
+		const updateRequest = await fetch(URL, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'appplication/json',
+				'X-Access-Key': API_KEY
+			},
+			body: JSON.stringify(bin)
+		});
+
+		console.log(await updateRequest.json())
+
+		return new Response("Finished");
 	},
 } satisfies ExportedHandler<Env>;
