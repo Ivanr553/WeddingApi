0c963154ecaff683789573be7bba1b3b4485b57e
Ivan <ivanr553@gmail.com>
2025-03-20 12:58:10 -0700

    adding in basic google sheets test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+	API_HOST: string;
+	ApiKey: string;
+	SheetId: string
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const SHEET_ID = env.SheetId;
+		const API_KEY = env.ApiKey;
+		const RANGE = "Sheet1!A1:D1";
+		const URL = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}:append?valueInputOption=USER_ENTERED&key=${API_KEY}`;
+
+		const data = {
+			values: [["New Value 1", "New Value 2", "New Value 3", "New Value 4"]]
+		};
+
+		const response = await fetch(URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data)
+		});
+
+		return new Response(await response.text(), {
+			headers: { "content-type": "application/json" }
+		});
 	},
 } satisfies ExportedHandler<Env>;
