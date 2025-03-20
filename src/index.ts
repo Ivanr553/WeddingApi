fb0f3ff0501c3f014c633cd811fa32bb836a2dd4
Ivan <ivanr553@gmail.com>
2025-03-20 14:58:06 -0700

    adding handleOptions method

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,57 @@ interface RSVP {
 	children: number;
 }
 
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+	"Access-Control-Max-Age": "86400",
+};
+
 export default {
+	//@ts-ignore
+	async handleOptions(request: Request): Promise<Response> {
+		if (
+			request.headers.get("Origin") !== null &&
+			request.headers.get("Access-Control-Request-Method") !== null &&
+			request.headers.get("Access-Control-Request-Headers") !== null
+		) {
+			// Handle CORS preflight requests.
+			return new Response(null, {
+				headers: {
+					...corsHeaders,
+					"Access-Control-Allow-Headers": request.headers.get(
+						"Access-Control-Request-Headers",
+					) ?? '*',
+				},
+			});
+		} else {
+			// Handle standard OPTIONS request.
+			return new Response(null, {
+				headers: {
+					Allow: "GET, HEAD, POST, OPTIONS",
+				},
+			});
+		}
+	},
 	async fetch(request, env, ctx): Promise<Response> {
+
+
+		if (
+			request.headers.get("Origin") !== null &&
+			request.headers.get("Access-Control-Request-Method") !== null &&
+			request.headers.get("Access-Control-Request-Headers") !== null
+		) {
+			// Handle CORS preflight requests.
+			return new Response(null, {
+				headers: {
+					...corsHeaders,
+					"Access-Control-Allow-Headers": request.headers.get(
+						"Access-Control-Request-Headers",
+					) ?? '*',
+				},
+			});
+		}
+
 		try {
 			const body: Body = await request.json();
 
